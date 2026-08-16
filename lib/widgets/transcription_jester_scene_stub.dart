b814d5acbfbb91b93f608a95576d826d7d5d1df4
DUPUIS-scot <james.hp.dy@gmail.com>
2026-08-16 01:03:42 +0100

    Add theatrical transcription jester scene

---
+++ lib/widgets/transcription_jester_scene_stub.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TranscriptionJesterScene extends StatelessWidget {
+  const TranscriptionJesterScene({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.expand();
+}
