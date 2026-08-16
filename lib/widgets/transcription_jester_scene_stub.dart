b3750370a33050c20b07737bb180692fbfa1d752
DUPUIS-scot <james.hp.dy@gmail.com>
2026-08-16 14:43:23 +0100

    Add selected-card no-op bridge for native builds

--- lib/widgets/transcription_jester_scene_stub.dart
+++ lib/widgets/transcription_jester_scene_stub.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class TranscriptionJesterScene extends StatelessWidget {
   const TranscriptionJesterScene({super.key});
 
+  static void setSelectedCard({
+    required String cardId,
+    required String imagePath,
+  }) {}
+
   @override
   Widget build(BuildContext context) => const SizedBox.expand();
 }
